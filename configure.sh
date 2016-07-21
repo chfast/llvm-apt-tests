17ada851c39e9f8165fd723da2d30d9e30f305d0
Paweł Bylica <chfast@gmail.com>
2016-07-21 23:27:25 +0200

    Help finding

--- configure.sh
+++ configure.sh
@@ -3,7 +3,7 @@ set -e
 
 case $SYSTEM in
     xenial) IMAGE='ubuntu:xenial' ;;
-    jessie) IMAGE='debian:jessie' ;;
+    jessie) IMAGE='debian:jessie'; LLVM_DIR='/usr/lib/llvm-3.8/share/llvm/cmake' ;;
 esac
 
 if [ $VERSION != 4.0 ]; then REPO_SUFFIX="-$VERSION"; fi
